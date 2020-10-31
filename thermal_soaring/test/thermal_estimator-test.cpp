a2d222b672cfb3a78d30bb4f5dfec41c11474e3d
Jaeyoung-Lim <jaeyoung@auterion.com>
2020-10-31 19:23:23 +0100

    Test and fix Thermal Estimator
    This commit adds testing to the thermal estimator and fixes the underlying issues with the estimator

---
+++ thermal_soaring/test/thermal_estimator-test.cpp
@@ -0,0 +1,36 @@
+#include "thermal_soaring/thermal_estimator.h"
+
+#include <gtest/gtest.h>
+#include <memory.h>
+#include <iostream>
+#include <ros/ros.h>
+
+class ThermalEstimatorTest : public ThermalEstimator, public ::testing::Test {
+ public:
+
+  void SetUp() override {
+
+  }
+
+  void TearDown() override {
+
+  }
+};
+
+TEST_F(ThermalEstimatorTest, TestPriorUpdate) {
+  Eigen::Vector4d state, predicted_state;
+  Eigen::Matrix4d covariance, predicted_covariance;
+
+  //No particular update in prior updates
+  PriorUpdate(state, covariance, predicted_state, predicted_covariance);
+  EXPECT_TRUE(state.isApprox(predicted_state));
+}
+
+TEST_F(ThermalEstimatorTest, TestMeasurementUpdate) {
+  Eigen::Vector4d state, predicted_state;
+  EXPECT_TRUE(true);
+}
+
+TEST_F(ThermalEstimatorTest, ObservationProcess) {
+  EXPECT_TRUE(true);
+}
